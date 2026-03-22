037bfc9d85806c01895fa82c7418d12c2e14f327
jktightwad <47039589+jktightwad@users.noreply.github.com>
2026-03-22 14:15:55 -0500

    Version 2.2.2

--- include/config.h
+++ include/config.h
@@ -3,7 +3,7 @@
 #include "secrets.h"
 
 // ===== VOLTMON VERSION =====
-#define VOLTMON_VERSION     "2.2.1"
+#define VOLTMON_VERSION     "2.2.2"
 
 // ===== COLLECTOR VERSION =====
 #define COLLECTOR_VERSION   "1.2.0"
